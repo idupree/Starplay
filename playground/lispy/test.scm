53851aa4fcd7d3675cff464aa6d869b69df84ed3
Isaac Dupree <antispam@idupree.com>
2013-03-12 22:17:40 -0400

    add to test case

--- playground/lispy/test.scm
+++ playground/lispy/test.scm
@@ -1,6 +1,9 @@
 (letrec
   ((S (lambda (x y z) (x z (y z))))
    (K (lambda (x y) x))
-   (I (lambda (x) x)))
-  ; this is a comment
+   (I (lambda (x) x))
+   ; hack since + etc isn't implemented yet:
+   (+ +) (* *) (= =) (- -)
+   (factorial (lambda (x) (if (= x 0) 1 (* x (factorial (- x 1))))))
+  )
   (I I))
